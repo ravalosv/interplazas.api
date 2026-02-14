1b59e00f2c304a3c1c3601d1b8b7b0986417d82a
Roberto Avalos <roberto.avalos@ikontechnologies.com>
2026-02-13 20:58:16 -0600

    fix: Update permissions for Email Templates and Filiales routes

--- src/routes/email-template.route.ts
+++ src/routes/email-template.route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { getItems, getItem, createItem, updateItem, deleteItem } from "../controller/email-template.controller";
-import { checkJwt } from "../core/middleware/session.middleware";
+import { checkJwt, checkRole } from "../core/middleware/session.middleware";
 
 const router = Router();
 
-router.get("/", checkJwt, getItems);
-router.get("/:id", checkJwt, getItem);
-router.post("/", checkJwt, createItem);
-router.put("/:id", checkJwt, updateItem);
-router.delete("/:id", checkJwt, deleteItem);
+router.get("/", checkJwt, checkRole([1, 3]), getItems);
+router.get("/:id", checkJwt, checkRole([1, 3]), getItem);
+router.post("/", checkJwt, checkRole([1, 3]), createItem);
+router.put("/:id", checkJwt, checkRole([1, 3]), updateItem);
+router.delete("/:id", checkJwt, checkRole([1, 3]), deleteItem);
 
 export { router };
--- src/routes/filial.route.ts
+++ src/routes/filial.route.ts
@@ -4,7 +4,7 @@ import { checkJwt, checkRole } from "../core/middleware/session.middleware";
 
 const router = Router();
 
-router.get("/", checkJwt, checkRole([1, 2, 3]), filialController.getFiliales);
+router.get("/", checkJwt, checkRole([1, 2, 3, 4]), filialController.getFiliales);
 router.get("/:id", checkJwt, checkRole([1, 3]), filialController.getFilial);
 router.post("/", checkJwt, checkRole([1, 3]), filialController.createFilial);
 router.put("/:id", checkJwt, checkRole([1, 3]), filialController.updateFilial);
